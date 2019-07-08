eb6188e78e333e3d87cad46f769e00b57f2b0714
Max Heckel <maxheckel@Maxs-MacBook-Pro.local>
2019-07-08 15:04:28 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module image_to_colors
+
+go 1.12
+
+require (
+	github.com/EdlinOrg/prominentcolor v1.0.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/oliamb/cutter v0.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/EdlinOrg/prominentcolor"
+	"github.com/gorilla/mux"
+	"image"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+var (
+	fileName    string
+	fullUrlFile string
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// IMPORTANT: you must specify an OPTIONS method matcher for the middleware to set CORS headers
+	r.HandleFunc("/colors", HandleGetColors).Methods(http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodOptions)
+	r.Use(mux.CORSMethodMiddleware(r))
+
+	http.ListenAndServe(":8081", r)
+
+
+
+}
+
+func HandleGetColors(w http.ResponseWriter, r *http.Request){
+	fullUrlFile = r.FormValue("image_url")
+
+	// Build fileName from fullPath
+	buildFileName()
+
+	// Create blank file
+	file := createFile()
+
+	// Put content on file
+	putFile(file, httpClient())
+	existingImageFile, err := os.Open(file.Name())
+
+	defer existingImageFile.Close()
+	defer os.Remove(file.Name())
+
+	// Calling the generic image.Decode() will tell give us the data
+	// and type of image it is as a string. We expect "png"
+	imageData, _, err := image.Decode(existingImageFile)
+	checkError(err)
+	res, err := prominentcolor.Kmeans(imageData)
+	checkError(err)
+	resp := &ColorResponse{}
+	resp.Colors = append(resp.Colors, "#"+res[0].AsString())
+	resp.Colors = append(resp.Colors, "#"+res[1].AsString())
+	resp.Colors = append(resp.Colors, "#"+res[2].AsString())
+	colorsJson, err := json.Marshal(resp)
+	checkError(err)
+	w.Write(colorsJson)
+
+}
+
+type ColorResponse struct{
+	Colors []string `json:"colors"`
+}
+
+func putFile(file *os.File, client *http.Client) {
+
+
+	request, _ := http.NewRequest("GET", fullUrlFile, nil)
+
+	request.Header.Set("User-Agent", "urvinyl.rocks")
+	resp, err := client.Do(request)
+
+	checkError(err)
+
+	defer resp.Body.Close()
+
+	size, err := io.Copy(file, resp.Body)
+
+	defer file.Close()
+
+	checkError(err)
+
+	fmt.Println("Just Downloaded a file %s with size %d", fileName, size)
+}
+
+func buildFileName() {
+	fileUrl, err := url.Parse(fullUrlFile)
+	checkError(err)
+
+	path := fileUrl.Path
+	segments := strings.Split(path, "/")
+
+	fileName = segments[len(segments)-1]
+}
+
+func httpClient() *http.Client {
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+
+	return &client
+}
+
+func createFile() *os.File {
+	file, err := os.Create(fileName)
+
+	checkError(err)
+	return file
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
